84493121db5a1dac8bcd3173edf3993cfe1862ef
Bharath Reddy <bharathreddy@MacBook-Pro-3.attlocal.net>
2025-09-18 14:32:11 -0500

    Fix dab_project as normal folder

---
+++ dab_project/tests/conftest.py
@@ -0,0 +1,20 @@
+# Run the tests from the root directory
+import sys
+import os
+import pytest
+
+sys.path.append(os.getcwd())
+
+# Returning a Spark Session
+@pytest.fixture()
+def spark():
+    try:
+        from databricks.connect import DatabricksSession
+        spark = DatabricksSession.builder.getOrCreate()
+    except ImportError:
+        try:
+            from pyspark.sql import SparkSession
+            spark = SparkSession.builder.getOrCreate()
+        except:
+            raise ImportError("Neither Databricks Session or Spark Session are available")
+    return spark
